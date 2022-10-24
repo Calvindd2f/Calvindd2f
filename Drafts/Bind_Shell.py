34173770810ce8b05f5c32b24cf394d37ad5513e
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-10-24 05:00:51 +0100

    Update Bind_Shell.py

--- Drafts/Bind_Shell.py
+++ Drafts/Bind_Shell.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-    import socket,os
+import socket,os,subprocess
 
-    # Enter IP address and port
-    HOST = '127.0.0.1'
-    PORT = 5000
-    # Configure socket connection
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    s.send('\ncat /home/calvin/test.txt\nEOFX'.encode())
+# Enter IP address and port
+HOST = '127.0.0.1'
+PORT = 5000
+# Configure socket connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.send('\ncat /home/calvin/test.txt\nEOFX'.encode())
 
-    while True:
+while True:
     # Receive data
     data = s.recv(1024)
     # Check for end of command
@@ -22,8 +22,7 @@
         if nextcmd == 'quit':
             s.send(nextcmd.encode())
             break
-        else: s.send(nextcmd.encode())
-        # If we haven't reach end of command, print
-        else:
-        print(data.decode())
-        
+            else: s.send(nextcmd.encode())
+                # If we haven't reach end of command, print
+                else:
+                    print(data.decode())
